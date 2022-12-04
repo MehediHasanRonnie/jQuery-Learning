443bcbd3aaaccb63d0e6a22641f2caf947023022
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-12-04 16:08:44 +0600

    css manipulation commit

---
+++ cssManupulation.js
@@ -0,0 +1,14 @@
+//setting color
+
+// $("h1").css("color", "red");
+// $("h1").css("font-size", "3rem"); //setting font size
+
+//we could do this all in one line using json string
+
+// $("h1").css({ color: "green", "font-size": "3rem", "font-weight": "5rem" });
+
+//the way we uss css style.css using javaScript
+
+// document.querySelector("h1").classList.add("style1");
+
+$("h1").addClass("style1 style2");
