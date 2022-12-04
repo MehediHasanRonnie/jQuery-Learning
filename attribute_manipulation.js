5e3c987a00ab21798e204b83e8e70ad454bd1615
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-12-04 15:41:54 +0600

    attribute manipulation using jQuery

---
+++ attribute_manipulation.js
@@ -0,0 +1,18 @@
+//getting attribute using javascript
+
+// var myAttr = document.querySelector("a").getAttribute("href");
+// console.log(myAttr);
+
+//same work with jQuery getting attribute
+
+// let myattr = $("a").attr("href");
+// console.log(myattr);
+
+//remove attribute
+
+// let myattr = $("a").removeAttr("href");
+// console.log(myattr);
+
+//set attribute
+
+$("a").attr("href", "http://yahoo.com");
